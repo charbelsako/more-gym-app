623c22bd28b493df27c89085bdd6bedfdc4b1fc4
Charbel Sarkis <charbelsako@gmail.com>
2024-08-09 16:47:56 +0300

    Fixes logout button and adds production server ip

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://10.0.2.2:5000',
+  baseURL: 'http://192.236.178.57:5000',
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: 'http://10.0.2.2:5000',
+  baseURL: 'http://192.236.178.57:5000',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
